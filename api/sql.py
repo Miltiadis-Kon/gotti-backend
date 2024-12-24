78f7e72910354bed9c92da0a6fa1b49572f6b157
Miltiadis-Kon <miltiadiskontos@gmail.com>
2024-12-24 13:30:47 +0200

    add minor log

--- api/sql.py
+++ api/sql.py
@@ -6,10 +6,12 @@ load_dotenv()
 
 
 def connect():
-    return psycopg2.connect(
+    conn = psycopg2.connect(
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("SUPABASE_CODE"),
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT")
     )
+    print("Connected to db")
+    return conn
